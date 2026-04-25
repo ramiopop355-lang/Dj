2233a21c3e32075bc1405751c59a71db399eaec6
Merada Wadjed nada elrayhane <wadjed.n.merada@ens-lagh.dz>
2026-04-25 19:55:14 +0100

    chore(twa): generate Android project for gradle-only build

---
+++ twa-android/app/src/main/java/com/sigma/education/dz/DelegationService.java
@@ -0,0 +1,14 @@
+package com.sigma.education.dz;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
